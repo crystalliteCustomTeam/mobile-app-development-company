8388e402f0c1a2401b94baf97c9c1b5a94a4b516
Ahmed-Crystallite3 <ahmed.r@crystallitedigital.com>
2025-02-17 23:51:03 +0500

    gtm robot & xml added

--- src/app/Scripts.jsx
+++ src/app/Scripts.jsx
@@ -74,10 +74,11 @@ const Scripts = () => {
     googleTagManager.strategy = "lazyOnload"
     googleTagManager.id = "google-tag-manager-GTM"
     googleTagManager.text = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-M46LFH3Z');`
+                          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                          })(window,document,'script','dataLayer','GTM-MNG58WB3')
+                          ;`
     document.head.appendChild(googleTagManager)
     //======== GATG ========//
     //     const gtmScript = document.createElement("script")
